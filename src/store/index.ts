0cbce1f91be8263cae792987292b3084dc2d8aad
Jeremias Binder <jeremiasb@gmx.de>
2019-12-28 17:34:02 +0100

    reverse commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
-import { combineReducers, createStore } from 'redux';
-import { demoReducer } from './demo/reducer';
-import { IDemoState } from './demo/types';
+import { combineReducers, createStore } from 'redux'
+import { demoReducer } from './demo/reducer'
+import { IDemoState } from './demo/types'
 
 export interface IRootState {
     demo: IDemoState
@@ -8,7 +8,8 @@ export interface IRootState {
 
 const store = createStore<IRootState, any, any, any>(
     combineReducers({
-        demo: demoReducer
-    }));
+        demo: demoReducer,
+    })
+)
 
-export default store;
+export default store
